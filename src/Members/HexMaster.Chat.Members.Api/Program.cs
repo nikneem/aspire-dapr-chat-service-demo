5373c41c63b57e464286d9f84ee04b43d4937c38
nikneem <ekeilholz+git@outlook.com>
2025-07-15 20:26:38 +0200

    Attempt

--- src/Members/HexMaster.Chat.Members.Api/Program.cs
+++ src/Members/HexMaster.Chat.Members.Api/Program.cs
@@ -1,6 +1,9 @@
 using HexMaster.Chat.Members.Abstractions.Interfaces;
 using HexMaster.Chat.Members.Abstractions.Requests;
+using HexMaster.Chat.Members.Api;
 using HexMaster.Chat.Members.Extensions;
+using Dapr.Client;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +12,26 @@ builder.AddServiceDefaults();
 // Add Azure Table Storage
 builder.AddAzureTableClient("tables");
 
+// Configure Dapr client with JSON serialization context for Native AOT
+builder.Services.AddSingleton<DaprClient>(serviceProvider =>
+{
+    var jsonOptions = new JsonSerializerOptions();
+    jsonOptions.TypeInfoResolverChain.Insert(0, MembersApiJsonSerializationContext.Default);
+    
+    var daprClientBuilder = new DaprClientBuilder()
+        .UseJsonSerializationOptions(jsonOptions);
+    
+    return daprClientBuilder.Build();
+});
+
 // Add chat members services
 builder.AddChatMembers();
+
+// Configure JSON serialization for Native AOT
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, MembersApiJsonSerializationContext.Default);
+});
 
 // Add CORS
 builder.Services.AddCors(options =>

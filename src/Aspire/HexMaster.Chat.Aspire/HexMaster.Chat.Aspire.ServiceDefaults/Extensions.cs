1bd0afdda6f58b8f22f2726c229b0b7d86f1d264
nikneem <ekeilholz+git@outlook.com>
2025-07-22 12:01:49 +0200

    Attempt

--- src/Aspire/HexMaster.Chat.Aspire/HexMaster.Chat.Aspire.ServiceDefaults/Extensions.cs
+++ src/Aspire/HexMaster.Chat.Aspire/HexMaster.Chat.Aspire.ServiceDefaults/Extensions.cs
@@ -17,21 +17,14 @@ public static class Extensions
     public static TBuilder AddServiceDefaults<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
     {
         builder.ConfigureOpenTelemetry();
-
         builder.AddDefaultHealthChecks();
-
         builder.Services.AddServiceDiscovery();
         builder.Services.AddDaprClient();
-
         builder.Services.ConfigureHttpClientDefaults(http =>
         {
-            // Turn on resilience by default
             http.AddStandardResilienceHandler();
-
-            // Turn on service discovery by default
             http.AddServiceDiscovery();
         });
-
         return builder;
     }
 

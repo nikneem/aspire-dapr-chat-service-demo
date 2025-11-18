4d32a3464eb3308e957adfec2d0bba6e510c08c7
nikneem <ekeilholz+git@outlook.com>
2025-11-18 10:18:46 +0100

    Refactor Dapr integration to utilize Redis and state store for microservices

--- src/Aspire/HexMaster.Chat.Aspire/HexMaster.Chat.Aspire.AppHost/Program.cs
+++ src/Aspire/HexMaster.Chat.Aspire/HexMaster.Chat.Aspire.AppHost/Program.cs
@@ -12,33 +12,56 @@ var storage = builder.AddAzureStorage("storage")
 var membersTables = storage.AddTables("memberstables");
 var messagesTables = storage.AddTables("messagestables");
 
-// Add Dapr
-builder.AddDapr();
+var redis = builder.AddRedis("redis").WithRedisInsight();
 
-var daprOptions = new DaprSidecarOptions
-{
-    ResourcesPaths = [Path.GetFullPath(Path.Combine(builder.AppHostDirectory, "../../../../dapr"))]
-};
+var stateStore = builder.AddDaprStateStore("chatservice-statestore")
+    .WaitFor(redis);
+
+var redisHost = redis.Resource.PrimaryEndpoint.Property(EndpointProperty.Host);
+var redisPort = redis.Resource.PrimaryEndpoint.Property(EndpointProperty.Port);
+
+var pubSub = builder
+  .AddDaprPubSub("chatservice-pubsub")
+  .WithMetadata(
+    "redisHost",
+    ReferenceExpression.Create(
+      $"{redisHost}:{redisPort}"
+    )
+  )
+  .WaitFor(redis);
+
 
 // Add microservices with Dapr sidecars
 var membersApi = builder.AddProject<Projects.HexMaster_Chat_Members_Api>(AspireConstants.MembersApiName)
     .WithReference(membersTables)
-    .WithDaprSidecar(daprOptions);
+    .WaitFor(membersTables)
+    .WaitFor(redis)
+    .WithDaprSidecar(sidecar =>
+    {
+        sidecar.WithReference(pubSub)
+            .WithReference(stateStore);
+    });
 
 var messagesApi = builder.AddProject<Projects.HexMaster_Chat_Messages_Api>(AspireConstants.MessagesApiName)
     .WithReference(messagesTables)
-    .WithDaprSidecar(daprOptions);
+    .WaitFor(messagesTables)
+    .WaitFor(redis)
+    .WithDaprSidecar(sidecar =>
+    {
+        sidecar.WithReference(pubSub)
+            .WithReference(stateStore);
+    });
 
 
 var realtimeApi = builder.AddProject<Projects.HexMaster_Chat_Realtime_Api>(AspireConstants.RealtimeApiName)
-    .WithDaprSidecar(daprOptions);
-
+    .WaitFor(redis)
+    .WithDaprSidecar(sidecar =>
+    {
+        sidecar.WithReference(pubSub);
+    });
 
 // Add Node.js Chat Client
 builder.AddNpmApp(AspireConstants.ChatClientName, "../../../ChatClient")
-    .WithReference(membersApi)
-    .WithReference(messagesApi)
-    .WithReference(realtimeApi)
     .WaitFor(membersApi)
     .WaitFor(messagesApi)
     .WaitFor(realtimeApi)
